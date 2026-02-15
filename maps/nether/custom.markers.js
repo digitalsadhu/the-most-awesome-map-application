3d3916e581d2892ed3054a3fe2d6d1338d9ea3cf
William Killerud <william@killerud.com>
2026-02-15 19:30:57 +0100

    add netherice markers from #1

--- maps/nether/custom.markers.js
+++ maps/nether/custom.markers.js
@@ -64,7 +64,108 @@ UnminedCustomMarkers = {
     isEnabled: true,
 
     markers: [
-
+        point({
+            x: -13,
+            z: 376,
+            text: "South",
+            type: 'netherice',
+        }),
+        point({
+            x: -15,
+            z: 662,
+            text: "Great Southern Ocean",
+            type: 'netherice',
+        }),
+        point({
+            x: -343,
+            z: 1189,
+            text: "Deep Down Diamonds",
+            type: 'netherice',
+        }),
+        point({
+            x: -21,
+            z: 1174,
+            text: "Geode",
+            type: 'netherice',
+        }),
+        point({
+            x: 732,
+            z: 640,
+            text: "The Forgotten Forest",
+            type: 'netherice',
+        }),
+        point({
+            x: -636,
+            z: 655,
+            text: "The Forbidden Lands",
+            type: 'netherice',
+        }),
+        point({
+            x: -438,
+            z: 380,
+            text: "Kensington Falls",
+            type: 'netherice',
+        }),
+        point({
+            x: -117,
+            z: -353,
+            text: "Northern Ocean",
+            type: 'netherice',
+        }),
+        point({
+            x: -14,
+            z: -928,
+            text: "The Northern Frontier",
+            type: 'netherice',
+        }),
+        point({
+            x: 218,
+            z: 42,
+            text: "East",
+            type: 'netherice',
+        }),
+        point({
+            x: 726,
+            z: 34,
+            text: "Razorback Hills",
+            type: 'netherice',
+        }),
+        point({
+            x: 730,
+            z: 475,
+            text: "Principalities of Shar",
+            type: 'netherice',
+        }),
+        point({
+            x: -417,
+            z: 41,
+            text: "West",
+            type: 'netherice',
+        }),
+        point({
+            x: -738,
+            z: 37,
+            text: "Westerly Rock",
+            type: 'netherice',
+        }),
+        point({
+            x: -745,
+            z: -383,
+            text: "Westport",
+            type: 'netherice',
+        }),
+        point({
+            x: -924,
+            z: 59,
+            text: "Acazniac Tribase",
+            type: 'netherice',
+        }),
+        point({
+            x: 758,
+            z: 252,
+            text: "Blaze\nFarm",
+            type: 'netherice',
+        }),
         // do not delete the following two closing brackets
     ]
 }
