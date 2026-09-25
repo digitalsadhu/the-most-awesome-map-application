de413a1386452cb1f72044d9433979cea46fab9e
William Killerud <william@killerud.com>
2026-09-25 07:36:11 +0200

    Update custom.markers.js

--- maps/end/custom.markers.js
+++ maps/end/custom.markers.js
@@ -4,6 +4,7 @@ uNmINeD does not overwrite this file during map generation.
 */
 
 const PINS = {
+    farm: { image: 'farm.png', name: "Farm" },
     other: { image: 'other.png', name: "Miscellaneous" },
 }
 
@@ -24,12 +25,18 @@ UnminedCustomMarkers = {
     isEnabled: true,
 
     markers: [
-        // point({
-        //     x: 758,
-        //     z: 252,
-        //     text: "New Flork",
-        //     type: 'city',
-        // }),
+        point({
+            x: 236,
+            z: -13,
+            text: 'Ender ender',
+            type: 'farm',
+        }),
+        point({
+            x: 3,
+            z: -220,
+            text: 'Shulker farm',
+            type: 'farm',
+        }),
         // do not delete the following two closing brackets
     ]
 }
